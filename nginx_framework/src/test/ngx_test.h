1d9152c62cc5163e24e5ca8cb0f3b81eed5cd40a
fangang.cheng <chengfangang@qq.com>
2015-09-06 16:32:16 +0800

    add listen conf

--- nginx_framework/src/test/ngx_test.h
+++ nginx_framework/src/test/ngx_test.h
@@ -14,5 +14,20 @@
 #include <ngx_event.h>
 #include <ngx_event_connect.h>
 
+typedef struct {
+	void **main_conf;
+	void **srv_conf;
+} ngx_test_conf_ctx_t;
+
+typedef struct {
+	u_char sockaddr[NGX_SOCKADDRLEN];
+	socklen_t socklen;
+	
+	ngx_test_conf_ctx_t *ctx;
+	
+	unsigned bind:1;
+	unsigned wildcard:1;
+	unsigned so_keepalive:2;
+} ngx_test_listen_t;
 
 #endif /* _NGX_MAIL_H_INCLUDED_ */
